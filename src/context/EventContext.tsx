5f2136d0e2b8c682a60125bf4fabe9fcbd3d83ca
unknown <malik.rizwan177@yahoo.com>
2024-12-07 20:27:57 +0500

    error removal try

--- src/context/EventContext.tsx
+++ src/context/EventContext.tsx
@@ -1,7 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
-import axios from "axios"
 
 const defaultValue = null
 
